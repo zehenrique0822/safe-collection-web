7e705d201898b17ea9c8fcc435e92a3a7c4120d7
José Henrique <zehenrique0822@gmail.com>
2023-03-04 01:33:30 -0300

    chore: add react-router-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+  build: { chunkSizeWarningLimit: 10000 },
+  envPrefix: ['REACT_APP_', 'VITE_'],
+  plugins: [
+    react()],
+  resolve: {
+    alias: {
+      emitter: 'events',
+      global: 'global/window',
+      crypto: 'crypto-browserify',
+      "@": path.resolve(__dirname, "./src")
+    },
+  },
+  server: {
+    host: true,
+    port: 3001,
+    hmr: {
+      clientPort: 3001
+    }
+  }
 })
